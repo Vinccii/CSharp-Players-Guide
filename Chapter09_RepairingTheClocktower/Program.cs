d0a097a30ddf981398ba0368b0d224b667e9e956
Vincent <vinciheinzelmann@gmail.com>
2025-06-08 14:20:50 +0200

    Add solution for "Repairing the Clocktower" challenge (if/else & modulo)

--- Chapter09_RepairingTheClocktower/Program.cs
+++ Chapter09_RepairingTheClocktower/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Zeit hier eingeben!");
+
+string input = Console.ReadLine();
+int time = Convert.ToInt32(input);
+
+if (time % 2 == 0)
+{
+    Console.WriteLine("Tick");
+}
+else
+{
+    Console.WriteLine("Tock");
+}
+
+    
+
