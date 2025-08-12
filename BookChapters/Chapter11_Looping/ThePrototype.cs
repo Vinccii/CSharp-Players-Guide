5f96de0afa223172d98fecacee034cc6d34c1959
Vincent <vinciheinzelmann@gmail.com>
2025-08-12 16:25:36 +0200

    Add ThePrototype challenge solution with do-while and while loops (Chapter 11)

---
+++ BookChapters/Chapter11_Looping/ThePrototype.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CsharpPlayersGuide.Chapter11_Looping
+{
+    public static class ThePrototype
+    {
+        public static void Run()
+        {
+            int pilotInput;
+            do
+            {
+                Console.WriteLine("Hello Pilot, please enter a Number between 0 and 100.");
+                pilotInput = Convert.ToInt32(Console.ReadLine());
+            }
+            while (pilotInput < 0 || pilotInput > 100);
+            Console.Clear();
+
+            Console.WriteLine("Hello Hunter, please guess a Number.");
+            {
+                while (true)
+                {
+                    int hunterInput = Convert.ToInt32(Console.ReadLine());
+
+                    if (hunterInput < pilotInput)
+                        Console.WriteLine($"{hunterInput} is too low.");
+                    else if (hunterInput > pilotInput)
+                        Console.WriteLine($"{hunterInput} is too high.");
+                    else
+                    {
+                        Console.WriteLine("You guessed the right number!");
+                        break;
+                    }
+                    Console.WriteLine("What is your next guess?");
+                }
+            }
+
+        }
+    }
+        
+}
--- BookChapters/Program.cs
+++ BookChapters/Program.cs
@@ -1,8 +1,10 @@
 ﻿//using CsharpPlayersGuide.Chapter08_Console_2_0;
 //using CsharpPlayersGuide.Chapter09_DecisionMaking;
-using CsharpPlayersGuide.Chapter10_Switches;
+//using CsharpPlayersGuide.Chapter10_Switches;
+using CsharpPlayersGuide.Chapter11_Looping;
 
 //DefenseOfConsolas.Run();
 // RepairingTheClocktower.Run();
 //Watchtower.Run();
-BuyingInventory.Run();
+//BuyingInventory.Run();
+ThePrototype.Run();
