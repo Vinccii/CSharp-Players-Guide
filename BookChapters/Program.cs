e132c09ead1c9fa49d6edf1902a28952ffe352c5
Vincent <vinciheinzelmann@gmail.com>
2025-08-19 22:50:09 +0200

    Add TakingANumber challange solution (Chapter 13)

--- BookChapters/Program.cs
+++ BookChapters/Program.cs
@@ -3,6 +3,7 @@
 //using CsharpPlayersGuide.Chapter10_Switches;
 using CsharpPlayersGuide.Chapter11_Looping;
 using CsharpPlayersGuide.Chapter12_Arrays;
+using CsharpPlayersGuide.Chapter13_Methods;
 
 //DefenseOfConsolas.Run();
 // RepairingTheClocktower.Run();
@@ -12,3 +13,4 @@ using CsharpPlayersGuide.Chapter12_Arrays;
 //TheMagicCannon.Run();
 //TheReplicatorOfDo_To.Run();
 //The_Laws_of_Freach.Run();
+TakingANumber.Run();
